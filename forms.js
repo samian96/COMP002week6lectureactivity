a4f5ffb0694bed74f22d5150c9e7ed517224a471
samian <sam.iannazzo@yahoo.com>
2025-11-19 23:08:51 -0800

    added innertext sessionstorage

--- forms.js
+++ forms.js
@@ -2,7 +2,13 @@ let form = document.querySelector("form");
 
 let textInput = document.getElementById("button1");
 
+let color = document.getElementById("color1");
+
 form.addEventListener("submit", event=> {
     event.preventDefault();
     console.log(textInput.value)
+    console.log(color.value)
+    color.innerText = sessionStorage.getItem("color1")
+    form.innerText = sessionStorage.getItem("form")
+    textInput.innerText = sessionStorage.getItem("button1")
 });
