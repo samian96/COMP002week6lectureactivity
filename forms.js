b8c5c190c261a9225ad57721cecc6d523a32ff59
samian <sam.iannazzo@yahoo.com>
2025-11-19 22:55:20 -0800

    added event listener

--- forms.js
+++ forms.js
@@ -1,3 +1,8 @@
 let form = document.querySelector("form");
 
 let textInput = document.getElementById("button1");
+
+form.addEventListener("submit", event=> {
+    event.preventDefault();
+    console.log(textInput.value)
+});
